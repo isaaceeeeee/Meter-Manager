0e4998b39a78bb78a4fb30bd16b39db151948abb
Isaac Ellard <104221346+isaaceeeeee@users.noreply.github.com>
2024-06-19 14:03:12 +0100

    Add files via upload

---
+++ Meter_Manager.py
@@ -0,0 +1,39 @@
+# Meter Manager 
+
+import requests
+import json
+import pandas as pd
+
+url = "https://www.meteronline.co.uk/api/v1/meter/meters"
+headers = {
+    "Accept": "application/json",
+    "Authorization": "AUTHORISATION_KEY"
+}
+
+response = requests.get(url, headers=headers)
+
+if response.status_code == 200:
+    data = response.json()  
+    print(json.dumps(data, indent=4))
+
+    df = pd.DataFrame(data)
+    print(df.head())
+else:
+    print(f"Error: {response.status_code} - {response.text}")
+
+url = f"https://www.meteronline.co.uk/api/v1/meter/{meter}/half-hourly-readings?from=2024-04-01T00%3A00&to=2024-04-30T23%3A30"
+headers = {
+    "Accept": "application/json",
+    "Authorization": "AUTHORISATION_KEY"
+}
+
+response = requests.get(url, headers=headers)
+
+if response.status_code == 200:
+    data = response.json()  
+    print(json.dumps(data, indent=4))
+    
+    df = pd.DataFrame(data)
+    print(df.head())
+else:
+    print(f"Error: {response.status_code} - {response.text}")
